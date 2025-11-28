922ea98a58baa4e7e223579b5aa371de4fac1c56
Nehalshaikh8698 <nehalshaikh02017@gmail.com>
2025-11-28 12:29:03 +0530

    batch route improve

--- backend/routes/batch.js
+++ backend/routes/batch.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: "Batch route working!" });
+  res.json({ message: "Batch is route working!" });
 });
 
 module.exports = router;
